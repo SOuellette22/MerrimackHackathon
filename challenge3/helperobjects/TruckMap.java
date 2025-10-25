5938e3f8e3638cfa8f782533ee867d5f1a8bd54f
SOuellette22 <ouellettese@merrimack.edu>
2025-10-25 15:32:15 -0400

    Fix: fixed issue with the Map object overriding Javas built in map function

--- challenge3/helperobjects/TruckMap.java
+++ challenge3/helperobjects/TruckMap.java
@@ -1,7 +1,15 @@
-public class Map{
+package helperobjects;
+
+import helperobjects.Location;
+
+import java.util.HashMap;
+import java.util.LinkedList;
+
+public class TruckMap {
     private HashMap<Integer, Location> locations;
     private LinkedList<Truck> trucks;
-    public Map(LinkedList<Truck> trucks, HashMap<Integer, Location> locations){
+
+    public TruckMap(LinkedList<Truck> trucks, HashMap<Integer, Location> locations){
         this.trucks = trucks;
         this.locations = locations;
     }
